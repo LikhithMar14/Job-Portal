df94f0a52674b34d998a78392bfc3a24262537db
2005LIKHITH <likhithcvsrl@gmail.com>
2025-01-02 20:33:03 +0530

    Commit

--- src/actions/Jobs.tsx
+++ src/actions/Jobs.tsx
@@ -1,11 +1,24 @@
 "use server"
 
 import { jobFilterSchema } from "@/types";
+import { redirect } from "next/navigation";
 
 const filterJobs = async(formdData:FormData) => {
     const values = Object.fromEntries(formdData.entries()); //javascript objects from form
 
-    const parseResult = jobFilterSchema.parse(values);
+    const {q,type,location,remote} = jobFilterSchema.parse(values);
+
+    const searchParams = new URLSearchParams({
+        ...(q && {q:q.trim()}),
+        ...(type && {type}),
+        ...(location && {location}),
+        ...(remote && {remote:"true"}),
+        
+
+    });
+    redirect(`/?${searchParams.toString()}`)
+
+    redirect
 }       
 
 export default filterJobs
