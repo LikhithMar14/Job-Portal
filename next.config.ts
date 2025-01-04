5f4ef725bef7244243305a37b3b5ea6756f0efeb
2005LIKHITH <likhithcvsrl@gmail.com>
2025-01-04 20:57:17 +0530

    Resolved Next js Images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['o2sedhxk4vqbwwiz.public.blob.vercel-storage.com'],
+  },
+
 };
 
 export default nextConfig;
